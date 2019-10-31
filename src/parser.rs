7a428598a7805df93fec6b57c480067e26034a98
Daniel Albert <esclear@users.noreply.github.com>
2019-10-31 23:31:42 +0100

    Simplify the parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,79 +1,35 @@
-
 use nom::{
-    IResult,
-    sequence::delimited,
-    character::complete::{char, digit1},
-    bytes::complete::{is_not, tag},
-    error::ErrorKind
+    character::complete::{char, digit1}
 };
 
 use crate::instructions::*;
 
-pub fn arithm_instruction(input: &str) -> IResult<&str, Instruction> {
-    let (input, target_register) = register(input)?;
-    
-    let (input, _) = tag(":=")(input)?;
-
-    let (input, left_operand)  = operand(input).map(|(s, o)| (s, Box::new(o)))?;
-    let (input, operator)      = operator(input)?;
-    let (input, right_operand) = operand(input).map(|(s, o)| (s, Box::new(o)))?;
-    
-    let (input, _) = tag(";")(input)?;
-
-    Ok((input, Instruction::Arithmetic { target_register, left_operand, operator, right_operand }))
-}
-
-pub fn conditional_jump(input: &str) -> IResult<&str, Instruction> {
-    // If keyword
-    let (input, _) = tag("if")(input)?;
-
-    // Condition
-    let (input, left_operand)  = operand(input)?;
-    let (input, relation)      = relation(input)?;
-    let (input, right_operand) = operand(input)?;
-    
-    // Keyword goto
-    let (input, _) = tag("goto")(input)?;
-    
-    // Address
-    let (input, address) = uint(input)?;
-    
-    let (input, _) = tag(";")(input)?;
-    
-    let cond_jump = Instruction::ConditionalJump {
-        left_operand: Box::new(left_operand),
-        relation,
-        right_operand: Box::new(right_operand),
-        target: address
-    };
-    
-    Ok((input, cond_jump))
-}
-
-
-pub fn register(input: &str) -> IResult<&str, Register> {
-    let (input, _)       = tag("R")(input)?;
-    let (input, address) = delimited(char('['), address, char(']'))(input)?;
-
-    Ok((input, Register { address }))
-}
-
-named!(address<&str, Address>, alt!(
-    uint     => { |addr| Address::Raw(addr) } |
-    register => { |reg|  Address::Register(Box::new(reg)) }
+named!(pub arithm_instruction<&str, Instruction>, map!(
+    tuple!(terminated!(register, tag!(":=")), operand, operator, operand),
+    |(target, left, op, right)| Instruction::Arithmetic { target_register: target, left_operand: Box::new(left), operator: op, right_operand: Box::new(right) }
 ));
 
-named!(uint<&str, u32>, map_res!(digit1, |s: &str| s.parse::<u32>()));
-named!(int<&str, i32>, map_res!(pair!(opt!(tag!("-")), uint),
-    |(p, abs): (Option<&str>, u32)|
-        Result::Ok::<i32, (&str, ErrorKind)>(if p.is_some() { -1 } else { 1 } * (abs as i32))
+named!(pub conditional_jump<&str, Instruction>, map!(
+    tuple!(preceded!(tag!("if"), operand), relation, terminated!(operand, tag!("goto")), uint),
+    |(left, rel, right, target)| Instruction::ConditionalJump { left_operand: Box::new(left), relation: rel, right_operand: Box::new(right), target }
 ));
+
+named!(register<&str, Register>, preceded!(tag!("R"), delimited!(char('['), address, char(']'))));
+
+named!(address<&str, Register>, alt!(
+    uint     => { |addr| Register::Address(addr) } |
+    register => { |reg|  Register::Register(Box::new(reg)) }
+));
+
+named!(uint<&str, u32>, flat_map!(digit1, parse_to!(u32)));
+named!(int<&str, i32>, flat_map!(recognize!(pair!(opt!(tag!("-")), uint)), parse_to!(i32)));
 
 named!(operator<&str, Operator>, alt!(
     tag!("+") => { |_| Operator::Plus  } |
     tag!("-") => { |_| Operator::Minus } |
     tag!("*") => { |_| Operator::Times } |
-    tag!("/") => { |_| Operator::Divides }
+    tag!("/") => { |_| Operator::Divides } |
+    tag!("div") => { |_| Operator::Divides }
 ));
 named!(relation<&str, Relation>, alt!(
     tag!("<")  => { |_| Relation::Lt  } |
